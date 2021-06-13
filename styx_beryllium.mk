ed0317565cb0d9a38768a8667bd0990c7483a694
humanglitch191 <humanglitch1.1@gmail.com>
2021-06-13 19:13:38 +0200

    beryllium: Adapt for StyxOS
    Signed-off-by: humanglitch191 <humanglitch1.1@gmail.com>

--- styx_beryllium.mk
+++ styx_beryllium.mk
@@ -6,17 +6,18 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common StyxOS stuff.
+$(call inherit-product, vendor/styx/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+STYX_BUILD_VARIANT := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := styx_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
-
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
